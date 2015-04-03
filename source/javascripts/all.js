fc08c40f421106756bad496a3d725a88b6c5da14
Bruno Chagas <brunochagasweb@gmail.com>
2015-04-03 13:50:43 -0300

    More styles on home-page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,18 @@
 SaraivaAtualiza = {} || SaraivaAtualiza;
+
+SaraivaAtualiza = {
+  setContentHeight: function(items) {
+    var elements = $(items),
+        sizes = [];
+
+    _.map(elements, function(el, index) {
+      sizes.push($(el).height());
+    });
+
+    height = Math.max.apply(Math, sizes);
+
+    _.map(elements, function(el, index) {
+      $(el).height(height);
+    });
+  }
+};
